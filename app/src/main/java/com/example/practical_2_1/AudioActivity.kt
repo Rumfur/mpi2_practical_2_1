28ce387ac049778c82ce48ca362635cbdac48abf
Pukitis <rovsvg@gmail.com>
2023-02-28 23:19:52 +0200

    Added function to record multiple audio files using shared preferences. Added display audio files function.

--- app/src/main/java/com/example/practical_2_1/AudioActivity.kt
+++ app/src/main/java/com/example/practical_2_1/AudioActivity.kt
@@ -1,17 +1,21 @@
 package com.example.practical_2_1
 
+import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.view.Menu
 import android.view.MenuItem
-import android.os.Environment
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.firebase.analytics.FirebaseAnalytics
@@ -19,19 +23,20 @@ import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 import java.io.File
 
+
 class AudioActivity : AppCompatActivity() {
 
-    val MICROPHONE_PERMISION_CODE = 200
+    private val MICROPHONE_PERMISION_CODE = 200
     private lateinit var analytics: FirebaseAnalytics
-    lateinit var mediaRecorder: MediaRecorder
-    lateinit var mediaPlayer: MediaPlayer
+    private lateinit var mediaRecorder: MediaRecorder
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio)
         analytics = Firebase.analytics
         if(checkMicrophone()){
-            getMicPermision()
+            getMicPermission()
         }
     }
 
@@ -49,17 +54,16 @@ class AudioActivity : AppCompatActivity() {
         return true
     }
 
-    fun onImage(){
+    private fun onImage(){
         val intent = Intent(this@AudioActivity, MainActivity::class.java)
         startActivity(intent)
     }
 
-
-    fun onDelete(){
+    private fun onDelete(){
         analytics.logEvent("audio_record_deleted", null)
     }
 
-    public fun startRecording(v: View) {
+    fun startRecording(v: View) {
         mediaRecorder = MediaRecorder()
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
@@ -71,14 +75,14 @@ class AudioActivity : AppCompatActivity() {
         analytics.logEvent("audio_record_started", null)
     }
 
-    public fun stopRecording(v: View) {
+    fun stopRecording(v: View) {
         mediaRecorder.stop()
         mediaRecorder.release()
         Toast.makeText(this, "Audio recording has stopped!", Toast.LENGTH_SHORT).show()
         analytics.logEvent("audio_record_saved", null)
     }
 
-    public fun playRecording(v: View) {
+    fun playRecording(v: View) {
         mediaPlayer = MediaPlayer()
         mediaPlayer.setDataSource(getRecordingFilePath())
         mediaPlayer.prepare()
@@ -86,23 +90,36 @@ class AudioActivity : AppCompatActivity() {
         Toast.makeText(this, "Playing audio!", Toast.LENGTH_SHORT).show()
     }
 
-    public fun checkMicrophone(): Boolean{
+    fun displayRecordedAudio(v: View){
+        val contextWrapper: ContextWrapper = ContextWrapper(applicationContext)
+        val files = File(contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC)!!.path).list()
+        val a = ArrayAdapter(this, android.R.layout.simple_list_item_1, files)
+
+        val listView = findViewById<ListView>(R.id.audioDisplay)
+        listView.setAdapter(a)
+    }
+
+    private fun checkMicrophone(): Boolean{
         if(this.packageManager.hasSystemFeature(PackageManager.FEATURE_MICROPHONE)){
             return true;
         }
         return false;
     }
 
-    private fun getMicPermision() {
+    private fun getMicPermission() {
         if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED){
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO), MICROPHONE_PERMISION_CODE)
         }
     }
 
-    public fun getRecordingFilePath(): String{
-        var contextWrapper: ContextWrapper = ContextWrapper(applicationContext)
-        var musicDirectory: File = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC)!!
-        var file: File = File(musicDirectory, "testRecordingFile" + ".mp3")
+    fun getRecordingFilePath(): String{
+        val sp = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
+        var editor = sp.edit()
+        editor.putInt("fileID", sp.getInt("fileID", 0) + 1)
+        val contextWrapper = ContextWrapper(applicationContext)
+        val musicDirectory: File = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC)!!
+        val file = File(musicDirectory, "testRecordingFile" + sp.getInt("fileID", 0) + ".mp3" )
+        editor.commit()
         return file.path
     }
 }
